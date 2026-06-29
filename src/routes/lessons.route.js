343822d3f800d44bd0fff6a6c498a6b30de18ccb
ushanrashmika23 <ushanrashmika23@gmail.com>
2026-06-29 21:38:57 +0530

    auth middleware sketched

--- src/routes/lessons.route.js
+++ src/routes/lessons.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const { authenticate } = require("../middlewares/auth.middleware");
 const lessonsController = require("../controllers/lessons.controller");
 
 router.post("/", lessonsController.newLessonController);
